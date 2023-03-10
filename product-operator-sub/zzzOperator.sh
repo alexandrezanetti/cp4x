32ffdb5498ff88f1055e671c17c045d3edac4ece
alexandrezanetti <alexandre_zanetti_almeida@yahoo.com.br>
2023-03-10 11:23:12 -0300

    Update zzzOperator.sh

--- product-operator-sub/zzzOperator.sh
+++ product-operator-sub/zzzOperator.sh
@@ -13,8 +13,8 @@ oc project $PROJECT
 mkdir -p /tmp/$PROJECT/$OPERATOR
 chmod 777 /tmp/$PROJECT/$OPERATOR
 
-cat /root/cp4i/product-operator-sub/product-operator.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/$PROJECT/g"  | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" | sed "s/{###PROVIDE_YOUR_SUBSCRIPTION_PROJECT_NAMESPACE_CP4X_HERE###}/$PROJECTSUB/g" >/tmp/$PROJECT/$OPERATOR-operator_OK.yaml
-cat /root/cp4i/product-operator-sub/product-sub.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_CHANNEL_OPERATOR_HERE###}/$CHANNEL/g" | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" | sed "s/{###PROVIDE_YOUR_SUBSCRIPTION_PROJECT_NAMESPACE_CP4X_HERE###}/$PROJECTSUB/g" >/tmp/$PROJECT/$OPERATOR-sub_OK.yaml
+cat /root/$PROJECT/product-operator-sub/product-operator.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/$PROJECT/g"  | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" | sed "s/{###PROVIDE_YOUR_SUBSCRIPTION_PROJECT_NAMESPACE_CP4X_HERE###}/$PROJECTSUB/g" >/tmp/$PROJECT/$OPERATOR-operator_OK.yaml
+cat /root/$PROJECT/product-operator-sub/product-sub.yaml | sed "s/{###PROVIDE_YOUR_PROJECT_NAMESPACE_CP4X_HERE###}/${PROJECT}/g" | sed "s/{###PROVIDE_CHANNEL_OPERATOR_HERE###}/$CHANNEL/g" | sed "s/{###PROVIDE_YOUR_OPERATOR_PRODUCT_CP4X_HERE###}/$OPERATOR/g" | sed "s/{###PROVIDE_YOUR_SUBSCRIPTION_PROJECT_NAMESPACE_CP4X_HERE###}/$PROJECTSUB/g" >/tmp/$PROJECT/$OPERATOR-sub_OK.yaml
 
 oc apply -f /tmp/$PROJECT/$OPERATOR-operator_OK.yaml
 oc apply -f /tmp/$PROJECT/$OPERATOR-sub_OK.yaml
